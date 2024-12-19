80fe57d2f3e20cd4dfa67f87d40a7006e1c91377
Osklundev <Oskarlundberg@hotmail.com>
2024-12-19 16:45:58 +0100

    🚧 Feat - Generate questions for play.

--- src/randomQuestions.ts
+++ src/randomQuestions.ts
@@ -1,30 +1,33 @@
-// Placeholder until array is created with all attributes.
-type Question = {
+interface Question {
   id: number;
-  question: string;
-  played: boolean;
-};
+  questionText: string;
+  answerOptionA: string;
+  answerOptionB: string;
+  answerOptionC: string;
+  correctAnswer: string;
+  isPlayed: boolean;
+}
 //                                   Input: Question Array    Return: Question Array
 export function getRandomQuestions(questionsArray: Question[]): Question[] {
-  const allPlayed = questionsArray.every(question => question.played); 
+  const allPlayed = questionsArray.every(question => question.isPlayed); 
   // Checks if all questions are played = true and then returns true if that's the case. 
   // So allPlayed will = true if every question has been played.
 
   if (allPlayed) {
     // Reset all questions to unplayed
     questionsArray.forEach(question => {
-      question.played = false;
+      question.isPlayed = false;
     });
   }
   // Takes all question that are not played = true
-  const availableQuestions = questionsArray.filter(question => !question.played); 
+  const availableQuestions = questionsArray.filter(question => !question.isPlayed); 
   // Shuffles the questions so they are random
   const shuffledQuestions = availableQuestions.sort(() => Math.random() - 0.5); 
   // Takes 10 questions from the shuffled array
   const selectedQuestions = shuffledQuestions.slice(0, 10); 
     selectedQuestions.forEach((question) => { 
       // Marks the selected questions as played = true
-        question.played = true; 
+        question.isPlayed = true; 
     });
     
     return selectedQuestions;
